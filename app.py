e6f7d1f177a3d63331711aa57b985ccbc6f8b44b
kevinclee26 <kevin.c.lee26@gmail.com>
2021-04-22 21:06:15 -0700

    ready for deployment

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+import os
+from dotenv import load_dotenv
+from c import PyMongo
+
+load_dotenv()
+
+DATABASE_URL=os.environ.get('DATABASE_URL')
+
+app=Flask(__name__)
+app.config['MONGO_URI'] = DATABASE_URL
+mongo = PyMongo(app)
+
+@app.route('/')
+def api_endpoint():
+	# mars = mongo.db.mars.find_one()
+	# results=collection.find()
+	# print(mongo.db.getCollectionNames())
+	# print(DATABASE_URL)
+	results=mongo.db.everything_14ers_db.find()
+	# for each_result in results[:5]: 
+		# print(each_result.__dict__)
+	# print(results[0])
+	# return jsonify([each_result['name'] for each_result in results])
+	return len(results)
+
+# @app.route('/insert')
+# def insert():
+# 	mongo.db.kevin_test_thank_you.insert({'name': 'this is a test'})
+# 	return 'SUCCESS'
+
+if __name__=='__main__': 
+	app.run()
