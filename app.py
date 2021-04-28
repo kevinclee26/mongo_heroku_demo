0fe1477b48df88ccf35964d4d6814125a509ab28
kevinclee26 <kevin.c.lee26@gmail.com>
2021-04-27 23:48:58 -0700

    updated

--- app.py
+++ app.py
@@ -47,12 +47,16 @@ def cnt_launches_per_site():
 @app.route('/all_launches_timeline')
 def all_launches_timeline(): 
 	results=client.mongo_db.launches.aggregate([
-		{'$match': {"links.mission_patch_small": { 
-                		"$exists": True, 
-                		'$ne': None}}}, 
+		# {'$match': {"links.mission_patch_small": { 
+  #               		"$exists": True, 
+  #               		'$ne': None}}}, 
 		{'$group': {'_id': {'year': {'$substr': ['$launch_date_utc', 0, 4]},
-							'month': {'$substr': ['$launch_date_utc', 5, 2]}},
-					'launches': {'$push': '$links.mission_patch_small'}}}]) 
+							'quarter': {'$ceil': {'$divide': [{'$toInt': {'$substr': ['$launch_date_utc', 5, 2]}}, 3]}}},
+					'launches': {'$push': {'mission_patch_small': '$links.mission_patch_small', 
+										   'mission_name': '$mission_name', 
+										   'video_link': '$links.video_link', 
+										   'wikipedia': '$links.wikipedia', 
+										   'mission_patch': '$links.mission_patch'}}}}]) 
 	return jsonify(list(results))
 
 @app.route('/all_launchpads')
--- static/js/chart.js
+++ static/js/chart.js
@@ -1,7 +1,8 @@
+// https://developer.mongodb.com/how-to/use-atlas-on-heroku/
 // Define SVG area dimensions
-var svgWidth = 1500;
+var svgWidth = 1600;
 // var svgHeight = 300;
-var svgHeight = svgWidth/6;
+var svgHeight = svgWidth/4;
 
 // Define the chart's margins as an object
 var chartMargin = {
@@ -16,10 +17,7 @@ var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
 var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
 // Select body, append SVG area to it, and set the dimensions
-var svg = d3.select("body")
-  .append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth);
+var svg = d3.select("svg")
 
 // Append a group to the SVG area and shift ('translate') it to the right and to the bottom
 var chartGroup = svg.append("g")
@@ -30,7 +28,7 @@ d3.json("/all_launches_timeline").then(function(launch_data) {
   var parseTime = d3.timeParse("%Y-%q");
   // Cast the hours value to a number for each piece of tvData
   launch_data.forEach(function(d) {
-    d.launch_date = parseTime(`${d._id.year}-${Math.ceil(d._id.month/3)}`);
+    d.launch_date = parseTime(`${d._id.year}-${d._id.quarter}`);
   });
 
   launch_data=launch_data.sort(function(a, b) {
@@ -44,12 +42,6 @@ d3.json("/all_launches_timeline").then(function(launch_data) {
     .range([0, chartWidth])
     .domain(d3.extent(launch_data, data => data.launch_date));
 
-  // Configure a band scale for the horizontal axis with a padding of 0.1 (10%)
-  // var xBandScale = d3.scaleBand()
-  //   .domain(launch_data.map(d => d.name))
-  //   .range([0, chartWidth])
-  //   .padding(0.1);
-
   // Create a linear scale for the vertical axis.
   var yLinearScale = d3.scaleLinear()
     .domain([0, d3.max(launch_data, d => d.launches.length)])
@@ -59,45 +51,87 @@ d3.json("/all_launches_timeline").then(function(launch_data) {
   // Create two new functions passing our scales in as arguments
   // These will be used to create the chart's axes
   var bottomAxis = d3.axisBottom(xTimeScale);
-  var leftAxis = d3.axisLeft(yLinearScale).ticks(4);
+  var leftAxis = d3.axisLeft(yLinearScale).ticks(d3.max(launch_data, d => d.launches.length));
+
+  var yScaleRange=yLinearScale.domain().slice(1).map((d, i)=>d-yLinearScale.domain()[i])
+  var xScaleRange=xTimeScale.domain().slice(1).map((d, i)=>d-xTimeScale.domain()[i])
+  var tick_size_y= chartHeight/yScaleRange
+  var tick_size_x= chartWidth/(xScaleRange/7884000000)
 
   // Append two SVG group elements to the chartGroup area,
   // and create the bottom and left axes inside of them
   chartGroup.append("g")
-    .call(leftAxis);
+        .attr('class', 'axis')
+        .call(leftAxis);
 
   chartGroup.append("g")
-    .attr("transform", `translate(0, ${chartHeight})`)
-    .call(bottomAxis);
+        .attr('class', 'axis')
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(bottomAxis);
 
-  // Create one SVG rectangle per piece of tvData
-  // Use the linear and band scales to position each rectangle within the chart
-  var groups=chartGroup.selectAll(".patch")
+  // Create one SVG rectangle per piece of launch_data
+  // Use the linear and time scales to position each rectangle within the chart
+  var groups=chartGroup.selectAll(".bar")
+  // chartGroup.selectAll(".patch")
                         .data(launch_data)
                         .enter()
                         .append('g')
-  var patches=groups.selectAll('.patch')
-                    .data(function(d) {
-                      return d.launches;
-                    })
-                    .enter()
-                    .append("svg:image")
-  patches.attr("y", (d, i) => yLinearScale(i)-30)
-         .attr("width", 30)
-         .attr("height", 30)
-         // .attr("x", function(d) {
-         //    return xTimeScale(d3.select(this.parentNode).datum().launch_date) 
-         // })
-         .attr('x', function(d) {
-           return xTimeScale(d3.select(this.parentNode).datum().launch_date)
-         })
-         .attr("xlink:href", d=>d);
-    // .attr("class", "bar")
-    // .attr("x", d => xTimeScale(d.launch_date))
-    // .attr("y", d => yLinearScale(d.launches.length))
-    // .attr("width", 5)
-    // .attr("height", d => chartHeight - yLinearScale(d.launches.length));
+  // groups.selectAll('.patch')
+  var rectangles=groups.append('rect')
+        .attr('y', d=>yLinearScale(d.launches.length))
+        .attr('x', d=>xTimeScale(d.launch_date))
+        .attr('width', tick_size_x)
+        .attr('height', d=>chartHeight-yLinearScale(d.launches.length))
+        .attr('fill', 'grey')
+        .attr('fill-opacity', 0.5)
 
+  var patches = groups.selectAll('.icon')
+        .data(function(d) {
+          return d.launches;
+        })
+        .enter()
+        .append("image")
+        .attr("y", (d, i) => yLinearScale(i)-tick_size_y)
+        .attr("width", tick_size_x)
+        .attr('height', tick_size_y)
+        .attr('x', function(d) {
+          // console.log(d)
+          return xTimeScale(d3.select(this.parentNode).datum().launch_date)
+        })
+        .attr('xlink:href', function(d) {return d.mission_patch_small? d.mission_patch_small: 'https://cdn.icon-icons.com/icons2/2221/PNG/512/logo_orange_spacex_icon_134372.png'})
+
+  // Step 1: Append a div to the body to create tooltips, assign it a class
+  // =======================================================
+  var toolTip = d3.select("body").append("div")
+    .attr("class", "tooltip");
+
+  // Step 2: Add an onmouseover event to display a tooltip
+  // ========================================================
+  groups.selectAll('image').on("click", function(e, d) {
+    toolTip.style("display", "block")
+           .style("left", (event.pageX - 50) + "px")
+           .style("top", (event.pageY - 100 )+ "px")
+    toolTip.append('p')
+           .text(d.mission_name)
+    toolTip.append('img')
+           .attr('class', 'tipImage')
+           .attr('src', d.mission_patch)
+    toolTip.append('hr')
+    toolTip.append('a')
+            .attr('href', d.wikipedia)
+            .append('p')
+            .text('Wikipedia')
+    toolTip.append('a')
+            .attr('href', d.video_link)
+            .attr('target', '_blank')
+            .append('p')
+            .text('Youtube')
+    })
+    // Step 3: Add an onmouseout event to make the tooltip invisible
+  toolTip.on("mouseleave", function() {
+    toolTip.html('')
+    toolTip.style("display", "none");
+  });
 }).catch(function(error) {
   console.log(error);
 });
