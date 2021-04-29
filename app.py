0c844239b0a1d299dad5e0e9947a3b530806442b
kevinclee26 <kevin.c.lee26@gmail.com>
2021-04-28 22:30:08 -0700

    updated

--- app.py
+++ app.py
@@ -1,88 +1,62 @@
-from flask import Flask, jsonify, render_template, redirect
-import os
-from dotenv import load_dotenv
+# /ZOCXZ2R6eN2q5VpD/
+
 import requests
+from dotenv import load_dotenv
+import os
 import pymongo
 
-load_dotenv()
+load_dotenv() # use dotenv to hide sensitive credential as environment variables
+# DATABASE_URL=os.environ.get('DATABASE_URL') # get connection url from environment
+DATABASE_URL='mongodb+srv://user:ZOCXZ2R6eN2q5VpD@mongo-heroku-cluster-we.kyrhz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-app=Flask(__name__)
+client=pymongo.MongoClient(DATABASE_URL) # establish connection with database
 
-DATABASE_URL=os.environ.get('DATABASE_URL') or 'mongodb://localhost:27017'
-
-client = pymongo.MongoClient(DATABASE_URL)
-
-@app.route('/')
-def home():
-	return render_template('index.html')
-
-@app.route('/all_launches')
-def launches(): 
-	results=client.mongo_db.launches.aggregate([
-		{'$lookup':{'from': "launchpads",
-			       'localField': "launch_site.site_id", 
-			       'foreignField': "site_id",
-			       'as': "launch_site"}}, 
-		{'$project': {'_id': 0, 
-					  'launch_site': {'_id': 0}}},  
-		{'$unwind': '$launch_site'}])
-	return jsonify(list(results))
-
-@app.route('/cnt_launches_per_site')
-def cnt_launches_per_site(): 
-	results=client.mongo_db.launches.aggregate([
-		{'$group': {'_id': '$launch_site.site_id', 
-					'count': {'$sum': 1}}}, 
-		{'$lookup': {'from': "launchpads",
-			        'localField': "_id", 
-			        'foreignField': "site_id",
-			        'as': "launch_site"}}, 
-		{'$unwind': '$launch_site'}, 
-		{'$project': {'_id': 0, 
-					  'launch_site': {'_id': 0},  
-					  'launch_site': {'location': 1, 'site_id': 1}, 
-					  'count': 1}}])
-	return jsonify(list(results))
-
-@app.route('/all_launches_timeline')
-def all_launches_timeline(): 
-	results=client.mongo_db.launches.aggregate([
-		# {'$match': {"links.mission_patch_small": { 
-  #               		"$exists": True, 
-  #               		'$ne': None}}}, 
-		{'$group': {'_id': {'year': {'$substr': ['$launch_date_utc', 0, 4]},
-							'quarter': {'$ceil': {'$divide': [{'$toInt': {'$substr': ['$launch_date_utc', 5, 2]}}, 3]}}},
-					'launches': {'$push': {'mission_patch_small': '$links.mission_patch_small', 
-										   'mission_name': '$mission_name', 
-										   'video_link': '$links.video_link', 
-										   'wikipedia': '$links.wikipedia', 
-										   'mission_patch': '$links.mission_patch'}}}}]) 
-	return jsonify(list(results))
-
-@app.route('/all_launchpads')
-def launchpads():
-	results=client.mongo_db.launchpads.find({}, {'_id': 0})
-	return jsonify(list(results))
-	
-@app.route('/get_token')
-def get_token():
-	return jsonify({'mapbox_token': os.environ.get('mapbox_token') or ''})
-
-@app.route('/refresh')
-def refresh():
-	client.mongo_db.launches.drop()
-	launches_url='https://api.spacexdata.com/v3/launches'
-	response=requests.get(launches_url)
-	data=response.json()
-	client.mongo_db.launches.insert(data)
-	client.mongo_db.launchpads.drop()
-	launchpads_url='https://api.spacexdata.com/v3/launchpads'
-	response=requests.get(launchpads_url)
-	data=response.json()
-	client.mongo_db.launchpads.insert(data)
-	# return 'SUCCESS'
+launches_url='https://api.spacexdata.com/v3/launches'
+response=requests.get(launches_url)
+if str(response.status_code)[0]=='2': # check for success
+	data=response.json() # get content in json format
+	client.mongo_db.launches.drop() # clear collection 
+	client.mongo_db.launches.insert(data) # insert all records
 	print('SUCCESS')
-	return redirect('/')
+else: # output for fail
+	print('FAILED')
 
-if __name__=='__main__': 
-	app.run()
+
+
+# from flask import Flask, jsonify, render_template, redirect
+# import os
+# from dotenv import load_dotenv
+# import requests
+# import pymongo
+
+# load_dotenv()
+
+# app=Flask(__name__)
+
+# DATABASE_URL=os.environ.get('DATABASE_URL') or 'mongodb://localhost:27017'
+
+# client = pymongo.MongoClient(DATABASE_URL)
+
+# @app.route('/')
+# def home():
+# 	return render_template('index.html')
+
+# @app.route('/refresh')
+# def refresh():
+# 	client.mongo_db.launches.drop()
+# 	launches_url='https://api.spacexdata.com/v3/launches'
+# 	response=requests.get(launches_url)
+# 	data=response.json()
+# 	client.mongo_db.launches.insert(data)
+# 	client.mongo_db.launchpads.drop()
+# 	launchpads_url='https://api.spacexdata.com/v3/launchpads'
+# 	response=requests.get(launchpads_url)
+# 	data=response.json()
+# 	client.mongo_db.launchpads.insert(data)
+# 	# return 'SUCCESS'
+# 	print('SUCCESS')
+# 	return redirect('/')
+
+
+# if __name__=='__main__': 
+# 	app.run()
